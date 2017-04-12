f54b25224c2405a7c41dd86e1afa76f9a025bc0e
DVfr <dvremix@gmail.com>
2017-04-12 10:28:28 +0200

    Views Ok

--- app/src/main/java/com/example/android/quizappproject/MainActivity.java
+++ app/src/main/java/com/example/android/quizappproject/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.android.quizappproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void submitAnswers(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Ca marche !!", Toast.LENGTH_SHORT);
+        toast.show();
+
+
+
+    }
+
+
 }
